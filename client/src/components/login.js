a8747f5a3eaf6e59b4ba4acff3990b479fec1fc0
JorgeCoronilla <jorge.coronilla.naranjo@gmail.com>
2023-03-09 11:50:09 +0100

    Untrack files in .gitignore

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -47,7 +47,7 @@ export const Login = ({setDisplay}) => {
                 <div><h5>Login</h5></div>
                 <div><input type="email" name='email' placeholder='Correo electrónico' required minLength="5" maxLength="40"></input>
                 </div>
-                <div> <input type="password" name='pass' required placeholder='Contraseña' minLength="4" maxLength="12"></input></div>
+                <div> <input type="password" name='pass' required placeholder='Contraseña' minLength="2" maxLength="12"></input></div>
                 <div><button type="submit">Log in</button></div>
             </form>
             <p onClick={recoverPass}>¿Olvidaste tu contraseña? Recupérala aquí.</p>
